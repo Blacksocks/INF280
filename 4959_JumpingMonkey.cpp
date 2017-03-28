3670b4c726278d20b9b14f9fdc85392c8274f1df
Vincent GAILLARD <vincent.gaillard@telecom-paristech.fr>
2017-03-28 16:48:17 +0200

    first version. Not totally ended. We must cheeck if a state n'a pas deja existe. Pour cela il faut utiliser le format binaire => etat de la foret = integer

--- 4959_JumpingMonkey.cpp
+++ 4959_JumpingMonkey.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string.h>
 #include <list>
+#include <climits>
 
 #define FOR(i,n)        for(int i = 0; i < n; i++)
 #define FOR3(m,i,n)     for(int i = m; i < n; i++)
@@ -26,6 +27,111 @@ int n; // number of nodes
 int m; // number of edges
 edge_t ** edges; // graph edges
 node_t ** nodes; // graph nodes
+list<int> treeFired;
+list<int> bestFired;
+int nb_fire = INT_MAX;
+
+/* value must be equal to node->value */
+void networkChangeValue(node_t * node, int newValue, int value)
+{
+    if(node->value == -1 || node->value != value)
+        return;
+    node->value = newValue;
+    FOR(i, node->nbEdges)
+        networkChangeValue(nodes[node->edges[i]], newValue, value);
+}
+
+int loopExists()
+{
+    int networkValue = 0;
+    // init graph
+    FOR(i, n)
+        nodes[i]->value = -1;
+    FOR(i, m)
+    {
+        int tmp1 = nodes[edges[i]->node1]->value;
+        int tmp2 = nodes[edges[i]->node2]->value;
+        if(tmp1 != -1 && tmp1 == tmp2)
+            return 1;
+        if(tmp1 == -1 && tmp2 == -1)
+        {
+            nodes[edges[i]->node1]->value = networkValue;
+            nodes[edges[i]->node2]->value = networkValue++;
+            continue;
+        }
+        if(tmp1 == -1)
+        {
+            nodes[edges[i]->node1]->value = nodes[edges[i]->node2]->value;
+            continue;
+        }
+        if(tmp2 == -1)
+        {
+            nodes[edges[i]->node2]->value = nodes[edges[i]->node1]->value;
+            continue;
+        }
+        networkChangeValue(nodes[edges[i]->node1], nodes[edges[i]->node2]->value, nodes[edges[i]->node1]->value);
+    }
+    return 0;
+}
+
+int update_values(int * nodes_val_save)
+{
+    int nbPos = 0;
+    FOR(i, n)
+        if(nodes_val_save[i] == 0)
+            FOR(j, nodes[i]->nbEdges)
+            {
+                nodes[nodes[i]->edges[j]]->value = 1;
+                nbPos++;
+            }
+    return nbPos;
+}
+
+void fire(int treeIdx)
+{
+#ifdef DEBUG
+    cout << "[";
+    for(std::list<int>::iterator it = treeFired.begin(); it != treeFired.end(); it++)
+        cout << *it << ",";
+    cout << "]" << endl;
+#endif
+    // attention, ne prend pas en compte l'ordre lexicographique
+    if((int)treeFired.size() >= nb_fire)
+        return;
+    int * nodes_val_save = new int[n];
+    // saving
+    FOR(i, n)
+        nodes_val_save[i] = nodes[i]->value;
+    FOR(i, n)
+        nodes[i]->value = 0;
+    treeFired.push_back(treeIdx);
+    nodes_val_save[treeIdx] = 0;
+    int nbPos = update_values(nodes_val_save);
+    cout << nbPos << endl;
+    // aucune position n'est possible pour le monkey
+    if(nbPos == 0)
+    {
+        // attention, ne prend pas en compte l'ordre lexicographique
+        if(nbPos < nb_fire)
+        {
+            nb_fire = nbPos;
+            // copy treeFired in bestFired
+            bestFired.clear();
+            for(std::list<int>::iterator it = treeFired.begin(); it != treeFired.end(); it++)
+                bestFired.push_back(*it);
+        }
+    }
+    else if(nbPos != n)
+    {
+        FOR(i, n)
+            fire(i);
+    }
+    treeFired.pop_back();
+    // charging
+    FOR(i, n)
+        nodes[i]->value = nodes_val_save[i];
+    delete[] nodes_val_save;
+}
 
 /* Solve Jumping Monkey
 ** https://icpcarchive.ecs.baylor.edu/external/20/2036.pdf
@@ -41,10 +147,7 @@ int main(void)
         nodes = new node_t*[n];
         edges = new edge_t*[m];
         FOR(i, n)
-        {
             nodes[i] = new node_t;
-            nodes[i]->value = 0;
-        }
         FOR(i, m)
             edges[i] = new edge_t;
         FOR(i, m)
@@ -69,7 +172,7 @@ int main(void)
             node->edges[node->nbEdges++] = edges[i]->node1;
         }
 #ifdef DEBUG
-        cout << "nbNodes: " << n << endl << "nbEdges: " << m << endl;
+        cout << endl << "nbNodes: " << n << endl << "nbEdges: " << m << endl;
         FOR(i, m)
             cout << "edge " << i << ": [" << edges[i]->node1 << "," << edges[i]->node2 << "] size: " << endl;
         FOR(i, n)
@@ -79,10 +182,22 @@ int main(void)
                 cout << nodes[i]->edges[j] << ",";
             cout << "]" << endl;
         }
-        cout << endl;
 #endif
         // algorithm
-
+        if(loopExists())
+        {
+            cout << "Impossible" << endl;
+            continue;
+        }
+        // Brute force
+        FOR(i, n)
+            nodes[i]->value = 1;
+        FOR(i, n)
+            fire(i);
+        cout << bestFired.size() << ":";
+        for(std::list<int>::iterator it = bestFired.begin(); it != bestFired.end(); it++)
+            cout << " " << *it;
+        cout << endl;
         // free
         FOR(i, n)
             delete[] nodes[i]->edges;

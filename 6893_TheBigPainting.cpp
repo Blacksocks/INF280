49078289d59a078f01c46e532d29162bfe629731
Blacksocks <20.100gaillard@gmail.com>
2017-04-04 23:18:10 +0200

    upgrade The Big Painting

--- 6893_TheBigPainting.cpp
+++ 6893_TheBigPainting.cpp
@@ -6,7 +6,7 @@
 #define FOR(i,n)        for(int i = 0; i < n; i++)
 #define FOR3(m,i,n)     for(int i = m; i < n; i++)
 
-#define DEBUG			1
+//#define DEBUG			1
 
 using namespace std;
 
@@ -34,6 +34,54 @@ inline void setPainting(int x, int y, int value)
 	painting[y * width + x] = value;
 }
 
+int knuth_morris_pratt()
+{
+	int * dist = new int[WIDTH * HEIGHT + 1];
+	int np = width;
+	dist[0] = -1;
+	int cnd = 0;
+	for (int i = 1; i <= np; i++)
+	{
+		dist[i] = cnd;
+		while (cnd >= 0 && painting[cnd] != painting[i])
+			cnd = dist[cnd];
+		cnd++;
+	}
+
+	int count = 0;
+	int ns = WIDTH * HEIGHT;
+	np = width;
+	cnd = 0;
+	for (int i = 0; i <= ns; i++)
+	{
+		while (cnd >= 0 && painting[cnd] != PAINTING[i])
+			cnd = dist[cnd];
+		cnd++;
+		if (cnd == np)
+		{
+			int idx = i - np + 1;
+			int x = idx % WIDTH;
+			int y = idx / WIDTH;
+			if(x + width > WIDTH)
+				goto stop;
+			if(y + height > HEIGHT)
+				break;
+			FOR3(1, k, height)
+				FOR(l, width)
+					if(getPainting(l, k) != getPAINTING(x + l, y + k))
+						goto stop;
+#ifdef DEBUG
+			printf("idx:%d, x:%d, y:%d\n", idx, x, y);
+#endif
+			count++;
+			stop:
+			cnd = dist[cnd];
+		}
+	}
+	delete[] dist;
+	return count;
+}
+
 /* Solve The Big Painting
 ** https://icpcarchive.ecs.baylor.edu/external/68/6893.pdf
 */
@@ -43,7 +91,7 @@ int main(void)
     while(1)
     {
         // input
-        if(!(cin >> width >> height >> WIDTH >> HEIGHT))
+        if(!(cin >> height >> width >> HEIGHT >> WIDTH))
             return 0;
         painting = new int[width * height];
         PAINTING = new int[WIDTH * HEIGHT];
@@ -60,20 +108,21 @@ int main(void)
                 setPAINTING(j, i, c == 'x');
             }
 #ifdef DEBUG
-	FOR(i, height)
-	{
-		FOR(j, width)
-			cout << (getPainting(j, i) ? 'x' : 'o');
-		cout << endl;
-	}
-	FOR(i, HEIGHT)
-	{
-		FOR(j, WIDTH)
-			cout << (getPAINTING(j, i) ? 'x' : 'o');
-		cout << endl;
-	}
-	cout << endl;
+		/*FOR(i, height)
+		{
+			FOR(j, width)
+				cout << (getPainting(j, i) ? 'x' : 'o');
+			cout << endl;
+		}*/
+		/*FOR(i, HEIGHT)
+		{
+			FOR(j, WIDTH)
+				cout << (getPAINTING(j, i) ? 'x' : 'o');
+			cout << endl;
+		}*/
+		//cout << endl;
 #endif
+		cout << knuth_morris_pratt() << endl;
 		delete[] painting;
 		delete[] PAINTING;
     }

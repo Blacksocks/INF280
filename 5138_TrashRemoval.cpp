d2f0349891a6196477f1ca5ea0c039d43493f347
Blacksocks <20.100gaillard@gmail.com>
2017-04-05 01:59:57 +0200

    improve trash removal

--- 5138_TrashRemoval.cpp
+++ 5138_TrashRemoval.cpp
@@ -7,13 +7,16 @@
 #define FOR(i,n)        for(int i = 0; i < n; i++)
 #define FOR3(m,i,n)     for(int i = m; i < n; i++)
 
-#define DEBUG			1
+//#define DEBUG			1
+
+#define MIN             (-FLT_MAX/2)
+#define MAX             (FLT_MAX/2)
 
 using namespace std;
 
 struct point {
     int x, y;
-    double slope;
+    float slope;
     point(): x(0), y(0) {slope = 0;}
     point(int in_x, int in_y): x(in_x), y(in_y) {slope = 0;}
     bool operator == (point pt) const {return (x == pt.x && y == pt.y);}
@@ -25,27 +28,29 @@ struct by_slope {
     }
 };
 
-double absolute(double x)
+float absolute(float x)
 {
     return(x >= 0 ? x : -x);
 }
 
-double vect(point p1, point p2, point p3)
+int vect(point p1, point p2, point p3)
 {
     int u1x = p2.x - p1.x;
     int u1y = p2.y - p1.y;
     int u2x = p3.x - p2.x;
     int u2y = p3.y - p2.y;
-    return (u1x * u2y - u1y * u2x) * 1.0;
+    return (u1x * u2y - u1y * u2x);
 }
 
 /* p1 and p2 form a line and this function return the distance between the line and p3
 */
-double getDist(point p1, point p2, point p3)
+float getDist(point p1, point p2, point p3)
 {
+#ifdef DEBUG
     printf("[%d,%d] [%d,%d] [%d,%d]\n", p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+#endif
     // ax + by + c = 0
-    double a, b, c;
+    float a, b, c;
     if(p2.x != p1.x)
     {
         a = (p2.y - p1.y) * 1.0 / (p2.x - p1.x);
@@ -54,13 +59,11 @@ double getDist(point p1, point p2, point p3)
     }
     else
     {
-        a = 0;
-        b = 1;
+        a = -1;
+        b = 0;
         c = p1.x;
     }
-    double res = absolute(a * p3.x + b * p3.y + c) / sqrt(a * a + b * b);
-    printf("a, b, c: %lf, %lf, %f\n", a, b, c);
-    printf("res: %lf\n", res);
+    float res = absolute(a * p3.x + b * p3.y + c) / sqrt(a * a + b * b);
     return res;
 }
 
@@ -78,14 +81,15 @@ list<point> getConvex(list<point> points)
     for(list<point>::iterator it = points.begin(); it != points.end(); it++)
     {
         int xdiff = (*it).x - minXpoint.x;
-        (*it).slope = xdiff == 0 ? ((*it).y > minXpoint.y ? DBL_MAX : DBL_MIN) : ((*it).y - minXpoint.y) * 1.0 / xdiff;
+        (*it).slope = (xdiff == 0 ? ((*it).y > minXpoint.y ? MAX : MIN) : ((*it).y - minXpoint.y) * 1.0 / xdiff);
     }
     points.sort(by_slope());
     points.push_front(minXpoint);
 #ifdef DEBUG
+    int i = 1;
     printf("ordered\n");
     for(list<point>::iterator it = points.begin(); it != points.end(); it++)
-        printf("[%d,%d] (%lf)\n", (*it).x, (*it).y, (*it).slope);
+        printf("%d [%d,%d] (%lf)\n", i++, (*it).x, (*it).y, (*it).slope);
 #endif
     // remove useless points
     list<point> result;
@@ -97,6 +101,12 @@ list<point> getConvex(list<point> points)
     {
         while(vect(*(prev(prev(result.end()))), result.back(), *it) < 0)
             result.pop_back();
+        if(vect(*(prev(prev(result.end()))), result.back(), *it) == 0)
+        {
+            result.pop_back();
+            while(vect(*(prev(prev(result.end()))), result.back(), *it) < 0)
+                result.pop_back();
+        }
         result.push_back(*it);
     }
     return result;
@@ -123,12 +133,9 @@ int main(void)
         }
 #ifdef DEBUG
         printf("===== %d =====\n", n);
-        int i = 0;
+        int i = 1;
         for(list<point>::iterator it = points.begin(); it != points.end(); it++)
-        {
-            i++;
-            printf("%d [%d,%d]\n", i, it->x, it->y);
-        }
+            printf("%d [%d,%d]\n", i++, it->x, it->y);
 #endif
         // algorithm
         points = getConvex(points);
@@ -137,39 +144,33 @@ int main(void)
         for(list<point>::iterator it = points.begin(); it != points.end(); it++)
             printf("[%d,%d]\n", (*it).x, (*it).y);
 #endif
-        double global_min_size = DBL_MAX;
+        float global_min_size = DBL_MAX;
         for(list<point>::iterator it = points.begin(); it != points.end(); it++)
         {
-            if(it == points.begin())
-                continue;
-            double max_size = 0;
-            for(list<point>::iterator it2 = it; it2 != points.end(); it2++)
+            /*if(it == points.begin())
+                continue;*/
+            float max_size = 0;
+            for(list<point>::iterator it2 = next(it); it2 != it; it2++)
             {
-                if(it2 == it)
-                    continue;
-                double tmp_max_size = getDist(*prev(it), *it, *it2);
-                printf("tmp_min_size: %lf / min_size: %lf\n", tmp_max_size, max_size);
+                if(it2 == points.end())
+                    it2 = points.begin();
+                point it_1 = (it == points.begin() ? points.back() : *prev(it));
+                float tmp_max_size = getDist(it_1, *it, *it2);
+#ifdef DEBUG
+                printf("tmp_max_size: %lf / max_size: %lf\n", tmp_max_size, max_size);
+#endif
                 if(tmp_max_size > max_size)
                     max_size = tmp_max_size;
                 else
                     break;
             }
-            //
-            printf("add\n");
-            double tmp_max_size = getDist(points.back(), *prev(prev(points.end())), points.front());
-            printf("tmp_min_size: %lf / min_size: %lf\n", tmp_max_size, max_size);
-            if(tmp_max_size > max_size)
-                max_size = tmp_max_size;
-            tmp_max_size = getDist(points.back(), points.front(), *prev(prev(points.end())));
-            printf("tmp_min_size: %lf / min_size: %lf\n", tmp_max_size, max_size);
-            if(tmp_max_size > max_size)
-                max_size = tmp_max_size;
-            //
+#ifdef DEBUG
             printf("min_size: %lf / global_min_size: %lf\n\n", max_size, global_min_size);
+#endif
             if(max_size < global_min_size)
                 global_min_size = max_size;
         }
-        cout << "Case " << idx++ << ": " << global_min_size << endl;
+        printf("Case %d: %.2lf\n", idx++, /*round(*/global_min_size/* * 100) / 100.0*/);
 
         // remove data
         points.clear();
